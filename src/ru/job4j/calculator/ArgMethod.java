4f9689bae254e7f1bbeea153eb83fccc93023e1f
2ilya2 <ilya_in_2@mail.ru>
2020-07-30 23:56:40 +1100

    4.1. Аргументы [#334832]

---
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -0,0 +1,23 @@
+package ru.job4j.calculator;
+
+public class ArgMethod {
+
+    public static void hello(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+    public static void myAge(int age) {
+        System.out.println("My age is , " + age);
+    }
+
+    public static void main(String[] args) {
+        String name = "Petr Arsentev";
+        int age = 33;
+
+        ArgMethod.hello(name);
+
+        ArgMethod.myAge(age);
+
+
+    }
+}
